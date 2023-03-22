f5893860ee5618d6f1503ca37d896a5f98f6a557
bykriz <cristianesquivelh@hotmail.com>
2023-03-22 16:51:31 -0400

    agregando los stats necesarios

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import { Cell } from "./components/Cell/Cell";
 import styles from "./App.module.scss";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [go, setGo] = useState("circle");
+  const [winnindMessage, setWinnindMessage] = useState(null);
+
+  const message = `It's now ${go}'s go.`;
 
   return (
     <div className="App">
       <div className={styles.gameboard}>
-        <Cell />
+        {cells.map((cell, index) => (
+          <Cell key={index} id={index} cell={cell} />
+        ))}
       </div>
-      <p></p>
+      <p>{message}</p>
     </div>
   );
 }
