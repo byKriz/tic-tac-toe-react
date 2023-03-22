bafe1f0669f9eaeb5cb73c5bad58c4cdaf7df516
bykriz <cristianesquivelh@hotmail.com>
2023-03-22 16:47:06 -0400

    agregndo props

--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import styles from "./Cell.module.scss"
+import React from "react";
+import styles from "./Cell.module.scss";
 
-export const Cell = () => {
+export const Cell = ({ id, cell }) => {
+
+  // console.log(cell);
+
   return (
-    <div className={styles.square}>
-    
+    <div className={styles.square} id={id}>
+      
     </div>
-  )
-}
+  );
+};
