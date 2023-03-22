dacb99f78f71d3666ebae9c2c80cb78894c90290
bykriz <cristianesquivelh@hotmail.com>
2023-03-22 16:36:32 -0400

    agregando estilos

--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -3,7 +3,7 @@ import styles from "./Cell.module.scss"
 
 export const Cell = () => {
   return (
-    <div className={styles.square}>Cell
+    <div className={styles.square}>
     
     </div>
   )
